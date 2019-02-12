34f865d2a5fd2b7cadbd190219a37105bcef8ce8
hesalk <42519593+hesalk@users.noreply.github.com>
2019-02-12 15:40:57 +0100

    add add bordbord funcc

--- model.js
+++ model.js
@@ -4,17 +4,16 @@ export default {
         return this._bords
     },
     addbord: function(bord){
+        let bord = {
+            id: id,
+        }
         _bords.push(bord)
-    },
-    newbord: {
-        bordid: 0,
-        bordname: "",
     },
     generateId: function(){
         let id = Math.floor(Math.random() * 1000000);
         let bords = this.getbord();
-        for(let card of bords){
-          if(card.id === id){
+        for(let bord of bords){
+          if(bord.id === id){
             return this.generateId();
           }
         }
